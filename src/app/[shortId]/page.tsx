57d1bbc6b45b2f4f4442cc5f0cd23b35dc602e1b
julienlemonnierr <julienlemonnierr@gmail.com>
2025-02-09 12:45:14 +0300

    Fix PageProps type and remove async

--- src/app/[shortId]/page.tsx
+++ src/app/[shortId]/page.tsx
@@ -1,15 +1,15 @@
 import { Metadata } from 'next';
 import ClientRedirectPage from './client';
 
-type Props = {
+interface PageProps {
   params: { shortId: string };
-};
+}
 
 export const metadata: Metadata = {
   title: 'Verification Required',
   description: 'Please verify that you are human',
 };
 
-export default function Page({ params }: Props) {
+export default function Page({ params }: PageProps) {
   return <ClientRedirectPage shortId={params.shortId} />;
 } 
